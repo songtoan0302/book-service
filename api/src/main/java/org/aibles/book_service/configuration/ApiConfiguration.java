24c993ea573e939bc39fa243d41e73d7813b08ff
Nguyễn Song Toàn <nguyensongtoan0302@gmail.com>
2022-10-02 17:54:59 +0700

    update module api:change dependency, remove anotation in class ApiConfiguration

--- api/src/main/java/org/aibles/book_service/configuration/ApiConfiguration.java
+++ api/src/main/java/org/aibles/book_service/configuration/ApiConfiguration.java
@@ -1,9 +1,10 @@
 package org.aibles.book_service.configuration;
 
+import org.aibles.core_exception.configuration.EnableCoreException;
 import org.springframework.context.annotation.Configuration;
 
+@EnableCoreException
 @Configuration
-@EnableCoreBusiness
 public class ApiConfiguration {
 
 }
